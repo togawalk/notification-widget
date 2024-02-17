5b7c6218cec192083d13b7c8c5ccd731daec2f7a
Evgeny <evgeny.efimenko@proton.me>
2024-02-17 22:17:59 +0500

    chore: new mixed background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { NotificationWidget } from "@/components/notification-widget";
 export default function Home() {
   return (
     <main className="bg-zinc-300 min-h-screen flex items-center justify-center relative">
-      <div className="absolute inset-0 bg-grid"/>
+      <div className="bg-lines absolute inset-0 bg-cover">
+        <div className="absolute inset-0 bg-grid" />
+      </div>
       <div className="w-full max-w-lg z-10">
         <NotificationWidget />
       </div>
